2342a2e4f771710eb89232538f656944cea7b31c
kthick0909 <87435678+kthick0909@users.noreply.github.com>
2023-08-04 05:07:17 +0530

    implement selected by genre

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import useData from "./useData";
+import { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
@@ -45,6 +46,9 @@ export interface Game {
 //   return { games, error, isLoading };
 // };
 
-const useGames = () => useData<Game>("/games");
+const useGames = (selectedGenre: Genre | null) =>
+  useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
+    selectedGenre?.id,
+  ]);
 
 export default useGames;
